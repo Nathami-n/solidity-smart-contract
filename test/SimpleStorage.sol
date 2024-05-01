62f832ede9529a6a64122b7ffb34b433d0a1bba2
Nathan <georgenathan010@gmail.com>
2024-05-01 16:59:01 +0300

    Added mapping(more like object but can only have distinct keys and values

--- test/SimpleStorage.sol
+++ test/SimpleStorage.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 
 contract SimpleStorage {
     uint256 myNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
   
     People[] public people;
 
@@ -14,6 +16,7 @@ contract SimpleStorage {
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
     function store (uint256 number) public {
         myNumber = number;
