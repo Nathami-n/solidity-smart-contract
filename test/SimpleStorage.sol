8340b86927f18e257de9aafa6106e087fc8ad121
Nathan <georgenathan010@gmail.com>
2024-05-02 11:41:02 +0300

    Added a conversion rate contract and a fundme function

--- test/SimpleStorage.sol
+++ test/SimpleStorage.sol
@@ -22,3 +22,28 @@ contract SimpleStorage {
         myNumber = number;
     }
 }
+
+contract FundMe {
+
+    uint256 public minUsd = 50;
+    function fund() public payable {
+        //set a minimum fund amount in usd
+        require(msg.value >= minUsd, "Error not enough funds");
+    }
+    
+    function getPrice() public view returns(uint256) {
+
+        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 answer,,,) =  priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+    //function withdraw(){}
+}
